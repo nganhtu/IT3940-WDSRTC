a3cb1424a9155b7577a88db9d1ed295ef9ee5b71
nganhtu <nganhtu0712@gmail.com>
2022-01-09 16:00:30 +0700

    remove port

--- src/main/resources/static/js/room.js
+++ src/main/resources/static/js/room.js
@@ -1,4 +1,4 @@
-const socket = io("https://wdsrtc.herokuapp.com:9092")
+const socket = io("https://wdsrtc.herokuapp.com")
 
 const myPeer = new Peer(undefined, {}) // use default server (PeerServer Cloud Service)
 
